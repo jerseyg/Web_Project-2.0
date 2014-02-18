3c197c9d42cff3c251556be0364dd9932a3d539d
Jersey Galapon <jerseyg@shaw.ca>
2014-02-18 14:46:54 -0500

    Trying to fix "Email.Email" name attribute error.

--- Web_Project2/Models/User.cs
+++ Web_Project2/Models/User.cs
@@ -8,11 +8,22 @@ namespace Web_Project2.Models
 {
     public class User
     {
+        [UIHint("EmailAddress")]
+
         public string EmailAddress { get; set; }
+
+        [UIHint("Password")]
+
         public string Password { get; set; }
+
         public string Salt { get; set; }
+        
+        [UIHint("FirstName")]
         public string FirstName { get; set; }
+
+        [UIHint("LastName")]
         public string LastName { get; set; }
+
         public int Role_ID { get; set; }
     }
 }
