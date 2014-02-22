9b30181d6d5ead33e11f3939d7e559b5388f5d50
Jersey Galapon <jerseyg@shaw.ca>
2014-02-22 01:38:57 -0600

    New Session Model

---
+++ Web_Project2/Models/SessionProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_Project2.Models
+{
+    [Serializable]
+    public class SessionProfile
+    {
+        public string parseID { get; set; }
+        public string EmailAddress { get; set; }
+        public string fullName { get; set; }
+    }
+}
