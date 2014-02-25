20c4e0647f6a43cc41bab8804559931dcda38ab6
Jersey Galapon <jerseyg@shaw.ca>
2014-02-25 00:50:14 -0600

    Fixed some layout problems

--- Web_Project2/App_Start/BundleConfig.cs
+++ Web_Project2/App_Start/BundleConfig.cs
@@ -8,8 +8,10 @@ namespace Web_Project2
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
             BundleTable.EnableOptimizations = true;
-            bundles.Add(new ScriptBundle("~/bundles/jquery", "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js").Include(
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.0.min.js").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
@@ -19,14 +21,14 @@ namespace Web_Project2
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js").Include(
                 "~/Content/js/*.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-            bundles.Add(new StyleBundle("~/Content/bootstrap",  "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css").Include(
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
                 "~/Content/css/bootstrap.css",
                 "~/Content/jumbotron.css"));
             bundles.Add(new StyleBundle("~/Content/Pixelkit").Include(
