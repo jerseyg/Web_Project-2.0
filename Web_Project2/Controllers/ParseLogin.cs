ea3cb10ae1d1ddc50c35156c7f32b1190c2b0e85
Jersey Galapon <jerseyg@shaw.ca>
2014-02-26 14:16:01 -0600

    Figured out custom Action attributes :D

---
+++ Web_Project2/Controllers/ParseLogin.cs
@@ -0,0 +1,33 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Web_Project2.Controllers
+{
+    class ParseLoginAttribute : ActionFilterAttribute, IActionFilter
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine("1");
+            if (ParseUser.CurrentUser != null)
+            {
+                Debug.WriteLine("Inside OnActionExecuting");
+                ViewResult result = new ViewResult();
+                result.ViewName = "index";
+                filterContext.Result = result;
+            }
+            else
+            {
+                Debug.WriteLine("2");
+                ViewResult result = new ViewResult();
+                result.ViewName = "Login";
+                result.ViewBag.ErrorMessage = "You are not authorized to use this page. Please contact administrator!";
+                filterContext.Result = result;
+            }
+        }
+    }
+}
