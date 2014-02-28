d812e962190035a55752897254c9425e8c21e7a9
unknown <Jersey.Galapon@Work-PC.Oasis.domain>
2014-02-28 14:17:18 -0600

    added UserREset Model to handle api calls

---
+++ Web_Project2/Models/UserReset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Project2.Models
+{
+    public class UserReset
+    {
+        public string emailAddress { get; set; }
+        public string token { get; set; }
+    }
+}
