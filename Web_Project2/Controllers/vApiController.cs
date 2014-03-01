9a3d2061bb12268a8cfb968d85d8c7aad35e39ed
Jersey Galapon <jerseyg@shaw.ca>
2014-03-01 13:07:58 -0600

    Testing of returning JSON

--- Web_Project2/Controllers/vApiController.cs
+++ Web_Project2/Controllers/vApiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Web_Project2.Models;
 
 namespace Web_Project2.Controllers
@@ -12,11 +13,14 @@ namespace Web_Project2.Controllers
         //
         // GET: /vApi/
         [HttpGet]
-        public ActionResult v1send(UserReset user)
+        public ActionResult v1(UserReset user)
         {
             ViewBag.Test1 = user.token;
             return View();
         }
-
+        public ActionResult SomeActionMethod()
+        {
+            return Json(new { foo = "bar", baz = "Blech" }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
