61cddf6860bef276681678f3446c4d0825b9b438
Jersey Galapon <jerseyg@shaw.ca>
2014-03-07 12:41:57 -0600

    Future use

---
+++ Web_Project2/Controllers/DatabaseHelper/ParseDb.cs
@@ -0,0 +1,69 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Web_Project2.ExternalHelper;
+
+namespace Web_Project2.Controllers.DatabaseHelper
+{
+    public class ParseDb
+    {
+        public async Task<Boolean> UserExists(string emailAddress)
+        {
+
+            var foundUser = await (from user in ParseUser.Query
+                                   where user.Get<string>("username") == emailAddress
+                                   select user).FindAsync();
+            return (foundUser.Count() != 0) ? true : false;
+        }
+
+        public async Task<ParseObject> GetSingleUserObject(string emailAddress)
+        {
+            var foundUser = await (from user in ParseUser.Query
+                                   where user.Get<string>("username") == emailAddress
+                                   select user).FindAsync();
+            var userId = foundUser.First();
+            return userId;
+        }
+        public string _Salt { get; set; }
+        public string HashPassword(string password)
+        {
+            var salt = PasswordHash.CreateSalt();
+            _Salt = salt;
+            byte[] byteArraySalt = Encoding.UTF8.GetBytes(salt);
+            var hash = PasswordHash.CreateHash(password, byteArraySalt);
+            return hash;
+        }
+        public string HashPassword(string password, string salt)
+        {
+            byte[] byteArraySalt = Encoding.UTF8.GetBytes(salt);
+            var hash = PasswordHash.CreateHash(password, byteArraySalt);
+            return hash;
+        }
+
+        public async Task<Boolean> login(string emailAddress, string password)
+        {
+            string email = emailAddress;
+            string nonHashedPassword = password;
+
+            try
+            {
+                var user = await GetSingleUserObject(email);
+                var salt = user.Get<string>("salt");
+                var hashedPassword = HashPassword(email, salt);
+
+                await ParseUser.LogInAsync(email, hashedPassword);
+
+                return true;
+            }
+            catch (ParseException)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Web_Project2/Models/UserTokenModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Project2.Models
+{
+    public class UserTokenModel
+    {
+        public string _UserId { get; set; }
+        public string _Token { get; set; }
+    }
+}
