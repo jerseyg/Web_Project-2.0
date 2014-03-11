f2a221c50bcfc1ccd336487c93f62d1a7bf464a9
Jersey Galapon <jerseyg@shaw.ca>
2014-03-11 17:28:00 -0500

    to upload and keep track of errors. Does not work

---
+++ Web_Project2/Controllers/ErrorHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web_Project2.Database;
+namespace Web_Project2.Controllers
+{
+    public class ErrorHandler : PDbContext
+    {
+        public void UploadError(Exception exception)
+        {
+            
+        }
+    }
+}
