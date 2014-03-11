b1a2b0a88afdd6f6155c44ccd3c0dde38228df6b
Jersey Galapon <jerseyg@shaw.ca>
2014-03-11 17:26:45 -0500

    Model for reset token

---
+++ Web_Project2/Models/ParseTokenModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Parse;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Project2.Models
+{
+    [ParseClassName("tokenAssociate")]
+    public class ParseTokenModel : ParseObject
+    {
+
+        [ParseFieldName("token")]
+        public string Token
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty<string>(value); }
+        }
+        [ParseFieldName("user")]
+        public string User
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty<string>(value); }
+        }
+    }
+}
